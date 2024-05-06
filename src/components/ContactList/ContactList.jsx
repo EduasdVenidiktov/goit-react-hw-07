cbb4282a7f9c1310a3bf3526c58513f9a09d1a12
Ed Ven <edven7777777@gmail.com>
2024-05-06 19:07:42 +0300

    correct errors  in contactlist &  contactOps

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,34 +1,18 @@
 import Contact from "../Contact/Contact";
 import SearchBox from "../SearchBox/SearchBox";
 import css from "./ContactList.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  selectFilteredContacts,
-  setSearchContact,
-} from "../../redux/contactsSlice";
+import { useSelector } from "react-redux";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 const ContactsList = () => {
   const searchContact = useSelector((state) => state.contacts.searchContact);
-
-  const dispatch = useDispatch();
   const filteredContacts = useSelector((state) =>
     selectFilteredContacts(state, searchContact)
   );
 
-  const handleInputChange = (event) => {
-    if (event && event.target && event.target.value) {
-      const { value } = event.target;
-      if (value && value.trim()) {
-        dispatch(setSearchContact(value.trim()));
-      } else {
-        dispatch(setSearchContact("")); // Очистити значення, якщо воно пусте
-      }
-    }
-  };
-
   return (
     <div>
-      <SearchBox value={searchContact} onChange={handleInputChange} />
+      <SearchBox value={searchContact} onChange={() => {}} />
 
       <ul className={css.contactList}>
         {filteredContacts.map((item) => (
